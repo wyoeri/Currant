a91a70d6fc3183dffec3e7c82e3a4307e4add00f
whyoeri <257165514+whyoeri@users.noreply.github.com>
2026-04-09 14:41:26 +0300

    feat: implement a gdt

---
+++ arch/i386/gdt.c
@@ -0,0 +1,28 @@
+#include "gdt.h"
+
+extern void gdt_flush(uint32_t gdt_ptr_addr);
+
+struct gdt_entry gdt[5];
+struct gdt_ptr gptr;
+
+void gdt_set_gate(int num, uint32_t base, uint32_t limit, uint8_t access, uint8_t gran){
+    gdt[num].limit_low = (limit & 0xFFFF);
+    gdt[num].base_low = (base & 0xFFFF);
+    gdt[num].base_middle = (base >> 16) & 0xFF;
+    gdt[num].access = access;
+    gdt[num].granularity = ((limit >> 16) & 0x0F) | (gran & 0xF0); 
+    gdt[num].base_high = (base >> 24) & 0xFF;
+}
+
+void gdt_init(void){
+    gptr.limit = (sizeof(struct gdt_entry) * 5) - 1;
+    gptr.base = (uint32_t)&gdt;
+
+    gdt_set_gate(0, 0, 0, 0, 0);
+    gdt_set_gate(1, 0, 0xFFFFFFFF, 0x9A, 0xCF);
+    gdt_set_gate(2, 0, 0xFFFFFFFF, 0x92, 0xCF);
+    gdt_set_gate(3, 0, 0xFFFFFFFF, 0xFA, 0xCF);
+    gdt_set_gate(4, 0, 0xFFFFFFFF, 0xF2, 0xCF);
+
+    gdt_flush((uint32_t)&gptr);
+}
