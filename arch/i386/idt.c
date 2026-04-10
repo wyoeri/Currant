7d4b10f925a2b03aa1f870a827dcd240d272896e
wyoeri <nnagornmaksim@gmail.com>
2026-04-10 21:38:24 +0300

    feat: implement idt

---
+++ arch/i386/idt.c
@@ -0,0 +1,75 @@
+#include "idt.h"
+
+#include "isr.h"
+
+extern void idt_load(uint32_t);
+
+struct idt_entry idt[256];
+struct idt_ptr iptr;
+
+void remap_pic(void){
+    outb(0x20, 0x11);
+    outb(0xA0, 0x11);
+    outb(0x21, 0x20);
+    outb(0xA1, 0x28);
+    outb(0x21, 0x04);
+    outb(0xA1, 0x02);
+    outb(0x21, 0x01);
+    outb(0xA1, 0x01);
+    outb(0x21, 0xFC);
+    outb(0xA1, 0xFF);
+}
+
+void idt_set_gate(int num, uint32_t base, uint16_t sel_code, uint8_t type_attr){
+    idt[num].base_lo = base & 0xFFFF;
+    idt[num].sel_code = sel_code;
+    idt[num].zero_bit = 0;
+    idt[num].type_attr = type_attr;
+    idt[num].base_hi = (base >> 16) & 0xFFFF;
+}
+
+void idt_init(void){
+    iptr.limit = (sizeof(struct idt_entry) * 256) - 1;
+    iptr.base = (uint32_t)&idt;
+
+    for(uint16_t i = 0; i < 256; i++){
+        idt_set_gate(i, 0, 0, 0);
+    }
+
+    idt_set_gate(0, (uint32_t)isr0, 0x08, 0x8E);
+    idt_set_gate(1, (uint32_t)isr1, 0x08, 0x8E);
+    idt_set_gate(2, (uint32_t)isr2, 0x08, 0x8E);
+    idt_set_gate(3, (uint32_t)isr3, 0x08, 0x8E);
+    idt_set_gate(4, (uint32_t)isr4, 0x08, 0x8E);
+    idt_set_gate(5, (uint32_t)isr5, 0x08, 0x8E);
+    idt_set_gate(6, (uint32_t)isr6, 0x08, 0x8E);
+    idt_set_gate(7, (uint32_t)isr7, 0x08, 0x8E);
+    idt_set_gate(8, (uint32_t)isr8, 0x08, 0x8E);
+    idt_set_gate(9, (uint32_t)isr9, 0x08, 0x8E);
+    idt_set_gate(10, (uint32_t)isr10, 0x08, 0x8E);
+    idt_set_gate(11, (uint32_t)isr11, 0x08, 0x8E);
+    idt_set_gate(12, (uint32_t)isr12, 0x08, 0x8E);
+    idt_set_gate(13, (uint32_t)isr13, 0x08, 0x8E);
+    idt_set_gate(14, (uint32_t)isr14, 0x08, 0x8E);
+    idt_set_gate(15, (uint32_t)isr15, 0x08, 0x8E);
+    idt_set_gate(16, (uint32_t)isr16, 0x08, 0x8E);
+    idt_set_gate(17, (uint32_t)isr17, 0x08, 0x8E);
+    idt_set_gate(18, (uint32_t)isr18, 0x08, 0x8E);
+    idt_set_gate(19, (uint32_t)isr19, 0x08, 0x8E);
+    idt_set_gate(20, (uint32_t)isr20, 0x08, 0x8E);
+    idt_set_gate(21, (uint32_t)isr21, 0x08, 0x8E);
+    idt_set_gate(22, (uint32_t)isr22, 0x08, 0x8E);
+    idt_set_gate(23, (uint32_t)isr23, 0x08, 0x8E);
+    idt_set_gate(24, (uint32_t)isr24, 0x08, 0x8E);
+    idt_set_gate(25, (uint32_t)isr25, 0x08, 0x8E);
+    idt_set_gate(26, (uint32_t)isr26, 0x08, 0x8E);
+    idt_set_gate(27, (uint32_t)isr27, 0x08, 0x8E);
+    idt_set_gate(28, (uint32_t)isr28, 0x08, 0x8E);
+    idt_set_gate(29, (uint32_t)isr29, 0x08, 0x8E);
+    idt_set_gate(30, (uint32_t)isr30, 0x08, 0x8E);
+    idt_set_gate(31, (uint32_t)isr31, 0x08, 0x8E);
+    idt_set_gate(32, (uint32_t)isr32, 0x08, 0x8E);
+    idt_set_gate(33, (uint32_t)isr33, 0x08, 0x8E);
+
+    idt_load((uint32_t)&iptr);
+}
---
+++ arch/i386/idt.h
@@ -0,0 +1,23 @@
+#ifndef IDT_H
+#define IDT_H
+
+#include "lib/types.h"
+
+struct idt_entry{
+    uint16_t base_lo;
+    uint16_t sel_code;
+    uint8_t zero_bit;
+    uint8_t type_attr;
+    uint16_t base_hi;
+} __attribute__((packed));
+
+struct idt_ptr{
+    uint16_t limit;
+    uint32_t base;
+} __attribute__((packed));
+
+void remap_pic(void);
+void idt_set_gate(int num, uint32_t base, uint16_t sel_code, uint8_t type_attr);
+void idt_init(void);
+
+#endif
