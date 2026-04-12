b02c60ef678b9e9d510066682a6fe24f3a182568
whyoeri <257165514+whyoeri@users.noreply.github.com>
2026-04-12 16:05:37 +0300

    refactor: remove color transfer from the vga function parameters

--- src/drivers/vga.c
+++ src/drivers/vga.c
@@ -2,14 +2,16 @@
 
 static uint8_t* const vga_buffer = (uint8_t*)0xB8000;
 static terminalcursor tcs;
+static uint8_t fill_color_vga;
 
 void init_vga(void){
     tcs.x = 0;
     tcs.y = 0;
+    set_fill_color_vga(0x0F);
 }
 
 // linear output
-void print_char_vga(const char c, uint8_t color_vga){
+void print_char_vga(const char c){
     if('\n' == c){
         tcs.x = 0; 
         tcs.y++;
@@ -20,13 +22,13 @@ void print_char_vga(const char c, uint8_t color_vga){
     else if('\b' == c && tcs.x > 0){
         tcs.x--;
         uint32_t offset = calc_offset_vga();
-        vga_buffer[offset] = '\0';
-        vga_buffer[offset + 1] = color_vga;
+        vga_buffer[offset] = ' ';
+        vga_buffer[offset + 1] = fill_color_vga;
     }
     else{
         uint32_t offset = calc_offset_vga();
         vga_buffer[offset] = c;
-        vga_buffer[offset + 1] = color_vga;
+        vga_buffer[offset + 1] = fill_color_vga;
         tcs.x++;
     }
 
@@ -42,34 +44,34 @@ void print_char_vga(const char c, uint8_t color_vga){
     }
 }
 
-void print_str_vga(const char* s, uint8_t color_vga){
+void print_str_vga(const char* s){
     while('\0' != *s){
-        print_char_vga(*s, color_vga);
+        print_char_vga(*s);
         s++;
     }
 }
 
 // output by coordinates
-void print_by_coord_char_vga(const char c, uint8_t color_vga, int y, int x){
+void print_by_coord_char_vga(const char c, int y, int x){
     tcs.y = y;
     tcs.x = x;
-    print_char_vga(c, color_vga);
+    print_char_vga(c);
 }
 
-void print_by_coord_str_vga(const char* s, uint8_t color_vga, int y, int x){
+void print_by_coord_str_vga(const char* s, int y, int x){
     tcs.y = y;
     tcs.x = x;
     while('\0' != *s){
-        print_char_vga(*s, color_vga);
+        print_char_vga(*s);
         s++;
     }
 }
 
 // other
-void clear_screen_vga(uint8_t color_vga){
+void clear_screen_vga(void){
     for(int i = 0; i < 80 * 25; i++){
         vga_buffer[i * 2] = ' ';
-        vga_buffer[i * 2 + 1] = color_vga;
+        vga_buffer[i * 2 + 1] = fill_color_vga;
     }
     tcs.x = 0;
     tcs.y = 0;
@@ -77,4 +79,8 @@ void clear_screen_vga(uint8_t color_vga){
 
 uint32_t calc_offset_vga(void){
     return (tcs.y * 80 + tcs.x) * 2;
+}
+
+void set_fill_color_vga(uint8_t color){
+    fill_color_vga = color;
 }
