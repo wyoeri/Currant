35806594728b0a1a6e0d1db218b1486c094d4f58
whyoeri <257165514+whyoeri@users.noreply.github.com>
2026-04-14 15:25:33 +0300

    feat: implement functions for working with strings

--- lib/string.h
+++ lib/string.h
@@ -1,6 +1,11 @@
 #ifndef STRING_H
 #define STRING_H
 
-int strcom(char* s1, char* s2);
+#include "lib/types.h"
+
+int strcom(const char* s1, const char* s2);
+int strlen(const char* s);
+void* memset(void* dest, int c, size_t n);
+void* memcpy(void* dest, const void* src, size_t n);
 
 #endif
