14be887793bb62cd0e9bf273b5f850abd874228f
wyoeri <nnagornmaksim@gmail.com>
2026-04-21 21:35:46 +0300

    feat: implement timer logic

---
+++ src/drivers/timer.h
@@ -0,0 +1,19 @@
+#ifndef TIMER_H
+#define TIMER_H
+
+#include "lib/other/types.h"
+
+extern volatile uint32_t timer_frequency;
+
+// working with a timer
+void init_timer(uint32_t frequency);
+void handler_timer(void);
+
+// working with the timer frequency
+void set_timer_frequency(uint32_t time);
+uint32_t get_timer_frequency(void);
+
+// working with time
+void sleep(uint32_t ms);
+
+#endif
