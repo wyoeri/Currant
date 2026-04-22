a0e39f2032c6a82bd87bc7b530fc0c61284d9125
wyoeri <nnagornmaksim@gmail.com>
2026-04-22 23:22:28 +0300

    refactor: clean up code terminal logic

--- src/terminal/terminal.c
+++ src/terminal/terminal.c
@@ -4,12 +4,15 @@
 #include "src/drivers/keyboard.h"
 #include "src/terminal/commands.h"
 
+#define SIZE_COL 25 // y
+#define SIZE_ROW 80 // x
+
 typedef struct{
     int x;
     int y;
-} terminalcursor;
+} terminalcursor_t;
 
-static terminalcursor tcs;
+static terminalcursor_t tcs;
 static char buffer_term[TERMINAL_BUFFER_SIZE];
 static int index_term = 0;
 static uint8_t theme_terminal = DEFAULT_THEME;
@@ -17,41 +20,18 @@ static uint8_t theme_terminal = DEFAULT_THEME;
 void init_terminal(void){
     tcs.x = 0;
     tcs.y = 0;
+    clear_screen();
+    output_invitation();
 }
 
 // linear printing
 void print_char(const char c){
-    if('\n' == c){
-        tcs.x = 0; 
-        tcs.y++;
-    }
-    else if('\t' == c){
-        tcs.x += 4;
-    }
-    else if('\b' == c && tcs.x > 0){
-        tcs.x--;
-        put_vga(' ', theme_terminal, tcs.x, tcs.y);
-    }
-    else{
-        put_vga(c, theme_terminal, tcs.x, tcs.y);
-        tcs.x++;
-    }
-
-    if (tcs.x >= 80) {
-        tcs.x = 0;
-        tcs.y++;
-    }
-
-    if (tcs.y >= 25) {
-        clear_screen();
-        tcs.x = 0;
-        tcs.y = 0;
-    }
+    char_handler(c);
 }
 
 void print_str(const char* s){
     while('\0' != *s){
-        print_char(*s);
+        char_handler(*s);
         s++;
     }
 }
@@ -60,14 +40,15 @@ void print_str(const char* s){
 void print_coord_char(const char c, int x, int y){
     tcs.y = y;
     tcs.x = x;
-    put_vga(c, theme_terminal, x, y);
+    put_vga(c, theme_terminal, tcs.x, tcs.y);
 }
 
 void print_coord_str(const char* s, int x, int y){
     tcs.y = y;
     tcs.x = x;
     while('\0' != *s){
-        put_vga(*s, theme_terminal, x, y);
+        put_vga(*s, theme_terminal, tcs.x, tcs.y);
+        tcs.x++;
         s++;
     }
 }
@@ -83,21 +64,68 @@ void input_processing_terminal(void){
     if(0 == c){return;}
 
     if('\n' == c){
-        print_char('\n');
+        char_handler('\n');
         buffer_term[index_term] = '\0';
         execute_command(buffer_term);
-        output_invitation();
         index_term = 0;
+        output_invitation();
+    }
+    else if('\t' == c){
+        char_handler('\t');
     }
     else if('\b' == c){
         if(index_term > 0){
            buffer_term[--index_term] = '\0';
-           print_char('\b'); 
+           char_handler('\b'); 
         }
     }
     else if(index_term < TERMINAL_BUFFER_SIZE - 1){
         buffer_term[index_term++] = c;
-        print_char(c); 
+        char_handler(c); 
+    }
+}
+
+void char_handler(const char c){
+    if('\n' == c){
+        tcs.x = 0; 
+        tcs.y++;
+    }
+    else if('\t' == c){
+        int new_x = (tcs.x + 4) & ~3;
+        if(new_x <  SIZE_ROW){
+            tcs.x = new_x;
+        }
+        else{
+            tcs.x = 0;
+            tcs.y++;
+        }
+    }
+    else if('\b' == c){
+        if(tcs.x > 0){
+            tcs.x--;
+            put_vga(' ', theme_terminal, tcs.x, tcs.y);
+        }
+    }
+    else if(' ' == c){
+        if(tcs.x < SIZE_ROW){
+            put_vga(' ', theme_terminal, tcs.x, tcs.y);
+            tcs.x++;
+        }
+    }
+    else{
+        if(tcs.x < SIZE_ROW){
+            put_vga(c, theme_terminal, tcs.x, tcs.y);
+            tcs.x++;
+        }
+    }
+
+    if(tcs.x >= SIZE_ROW){
+        tcs.x = 0;
+        tcs.y++;
+    }
+
+    if(tcs.y >= SIZE_COL){
+        clear_screen();
     }
 }
 
@@ -117,12 +145,12 @@ uint8_t make_color_terminal(uint8_t fg, uint8_t bg){
 
 // other
 void output_invitation(void){
-    print_str("$ ");
+    print_str("> ");
 }
 
 void clear_screen(void){
-    for(int i = 0; i < 25; i++){
-        for(int j = 0; j < 80; j++){
+    for(int i = 0; i < SIZE_COL; i++){
+        for(int j = 0; j < SIZE_ROW; j++){
             put_vga(' ', theme_terminal, j, i);
         }
     }
