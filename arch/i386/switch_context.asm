37e293ca334951c0f8f801e6418eb237663d2c57
whyoeri <257165514+whyoeri@users.noreply.github.com>
2026-04-25 21:25:06 +0300

    feat: implement logic scheduler

---
+++ arch/i386/switch_context.asm
@@ -0,0 +1,22 @@
+[BITS 32]
+
+section .text
+global switch_context
+switch_context:
+    push ebp
+    push ebx
+    push esi
+    push edi
+
+    mov eax, [esp + 20]
+    mov edx, [esp + 24]
+
+    mov [eax], esp
+    mov esp, edx
+
+    pop edi
+    pop esi
+    pop ebx
+    pop ebp
+
+    ret
